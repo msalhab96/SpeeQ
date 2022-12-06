c482024458dab3e76274e233753a147d948155b9
msalhab96 <mahmoud@salhab.work>
2022-12-06 13:14:01 +0300

    VolumeChanger added

--- data/augmenters.py
+++ data/augmenters.py
@@ -21,3 +21,34 @@ class WhiteNoiseInjector(StochasticProcess):
     def func(self, x: Tensor) -> Tensor:
         gain = random.random()
         return x + gain * torch.randn_like(x).to(x.device)
+
+
+class VolumeChanger(StochasticProcess):
+    """Changes the amplitude of the input signal by
+    a random gain.
+
+    Args:
+        ratio (float): The ratio/rate that the augmentation will be
+        applied to the data. Default 1.0
+        min_gain (float): The minimum gain that will be multiplied by
+        the signal.
+        max_gain (float): The maximum gain that will be multiplied by
+        the signal.
+    """
+    def __init__(
+            self,
+            min_gain: float,
+            max_gain: float,
+            ratio=1.0
+            ) -> None:
+        super().__init__(ratio)
+        self.min_gain = min_gain
+        self.max_gain = max_gain
+        self._diff = self.max_gain - self.min_gain
+
+    @property
+    def _gain(self):
+        return self._diff * random.random() + self.min_gain
+
+    def func(self, x: Tensor) -> Tensor:
+        return self._gain * x
