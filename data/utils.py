77eefa3f2d0907ec628999591afb3a056d8d5e34
msalhab96 <mahmoud@salhab.work>
2022-12-07 08:25:47 +0300

    save_text and save_json added

--- data/utils.py
+++ data/utils.py
@@ -1,4 +1,5 @@
 import json
+from typing import Union
 
 
 def load_json(file_path, encoding='utf-8'):
@@ -11,3 +12,21 @@ def load_text(file_path, encoding='utf-8'):
     with open(file_path, 'r', encoding=encoding) as f:
         data = f.read()
     return data
+
+
+def save_json(
+        file_path,
+        data: Union[dict, list],
+        encoding='utf-8'
+        ) -> None:
+    with open(file_path, 'w', encoding=encoding) as f:
+        json.dump(data, f)
+
+
+def save_text(
+        file_path,
+        data: str,
+        encoding='utf-8'
+        ) -> None:
+    with open(file_path, 'w', encoding=encoding) as f:
+        f.write(data)
