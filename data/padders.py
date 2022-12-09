e6907fcbfe57342126321701dce5f47c640d95d1
msalhab96 <mahmoud@salhab.work>
2022-12-09 14:05:36 +0300

    DynamicPadder added

---
+++ data/padders.py
@@ -0,0 +1,44 @@
+import torch
+from .interfaces import IPadder
+from torch import Tensor
+from typing import Union
+
+
+class DynamicPadder(IPadder):
+    """Pads the input sequence across a dim for the maximum length
+
+    Args:
+        dim (int): The dimension to do the padding across.
+        pad_val (Union[int, Tensor, float]): The padding value that
+        will be used to fill the padding sequence.
+        left_pad (int): The side to pad the padding sequence to.
+    """
+    def __init__(
+            self,
+            dim: int,
+            pad_val: Union[int, Tensor, float],
+            left_pad=False
+            ) -> None:
+        super().__init__()
+        self.dim = dim
+        self.left_pad = left_pad
+        self.pad_val = pad_val
+
+    def pad(self, x: Tensor, max_len: int):
+        seq_len = x.shape[self.dim]
+        pad_len = max_len - seq_len
+        assert pad_len >= 0
+        if pad_len == 0:
+            return x, pad_len
+        pad = torch.zeros(
+            *x.shape[:self.dim],
+            pad_len,
+            *x.shape[1 + self.dim:],
+            dtype=x.dtype
+            ).to(x.device)
+        pad = pad + self.pad_val
+        if self.left_pad:
+            x = torch.cat([pad, x], dim=self.dim)
+        else:
+            x = torch.cat([x, pad], dim=self.dim)
+        return x, pad_len
