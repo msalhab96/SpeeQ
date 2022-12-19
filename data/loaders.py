41e8d942e5e2aed0003326b968ac3e632e7d63d1
msalhab96 <mahmoud@salhab.work>
2022-12-19 08:30:39 +0300

    SpeechTextDataset added

--- data/loaders.py
+++ data/loaders.py
@@ -1,6 +1,11 @@
-from typing import Union
+import torch
+from torch import Tensor
+from typing import Tuple, Union
 from pathlib import Path
 from .utils import load_csv
+from .processors import IProcessor
+from .interfaces import ITokenizer
+from constants import FileKeys
 
 
 class CSVDataset:
@@ -32,3 +37,55 @@ class CSVDataset:
 
     def __len__(self) -> int:
         return len(self.data)
+
+
+class SpeechTextDataset(CSVDataset):
+    """Implements the basic dataset for speech-text pairs
+    for speech-recognition application.
+
+    Args:
+        data_path (Union[str, Path]): The file path.
+        tokenizer (ITokenizerITokenizer): The tokenizer that will be used.
+        speech_processor (IProcessor): The speech processor.
+        text_processor (IProcessor): The text processor.
+        sep (str): The CSV separator.
+        encoding (str): The file encoding. Default "utf-8".
+    """
+    def __init__(
+            self,
+            data_path: Union[str, Path],
+            tokenizer: ITokenizer,
+            speech_processor: IProcessor,
+            text_processor: IProcessor,
+            sep: str,
+            encoding='utf-8'
+            ) -> None:
+        super().__init__(
+            data_path, sep, encoding
+            )
+        self.tokenizer = tokenizer
+        self.speech_processor = speech_processor
+        self.text_processor = text_processor
+
+    def process_text(self, text: str) -> Tuple[Tensor, int]:
+        text = self.text_processor.execute(text)
+        # FIX: eos and sos for CTC-based model
+        tokens = self.tokenizer.tokenize(
+            text, add_sos=True, add_eos=True
+            )
+        return torch.LongTensor(tokens), len(tokens)
+
+    def process_speech(
+            self, file_path: Union[Path, str]
+            ) -> Tensor:
+        speech = self.speech_processor.execute(file_path)
+        # FIX: handle MONO and STEREO issue
+        return speech, speech.shape[-2]
+
+    def __getitem__(self, idx: int) -> dict:
+        item = super().__getitem__(idx)
+        text, text_len = self.process_text(item[FileKeys.text_key.value])
+        speech, speech_len = self.process_speech(
+            item[FileKeys.speech_key.value]
+            )
+        return speech, speech_len, text, text_len
