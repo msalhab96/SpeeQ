c5b4948c074b243ae8cf2b0d847efb4b6bcf25f1
msalhab96 <mahmoud@salhab.work>
2022-12-25 17:22:57 +0300

    constants.py updated

--- constants.py
+++ constants.py
@@ -5,3 +5,22 @@ class FileKeys(Enum):
     text_key = 'text'
     speech_key = 'file_path'
     duration_key = 'duration'
+
+
+class StateKeys(Enum):
+    history = 'history'
+    epoch = 'epoch'
+    model = 'model'
+    step = 'step'
+    optimizer = 'optimizer'
+
+
+class HistoryKeys(Enum):
+    train_loss = 'train_loss'
+    test_loss = 'test_loss'
+
+
+class LogCategories(Enum):
+    batches = 'batches'
+    steps = 'steps'
+    epochs = 'epochs'
