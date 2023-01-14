733d27969fb79b40839c8cc2ede888341dc8beee
msalhab96 <mahmoud@salhab.work>
2023-01-14 12:11:04 +0300

    results pre pre-processing functions added

--- trainers/criterions.py
+++ trainers/criterions.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+from torch import Tensor
 from torch import nn
 
 
@@ -15,3 +17,31 @@ class CTCLoss(nn.CTCLoss):
             reduction=reduction,
             zero_infinity=zero_infinity
             )
+
+
+def remove_positionals(
+        input: Tensor, target: Tensor
+        ) -> Tuple[Tensor, Tensor]:
+    """Removes the SOS from the target and EOS
+    prediction from the input
+
+    Args:
+        input (Tensor): The input tensor of shape [B, M, C].
+        target (Tensor): The target tensor of shape [B, C]
+
+    Returns:
+        Tuple[Tensor, Tensor]: The input and target.
+    """
+    input = input[:, :-1, :]
+    input = input.contiguous()
+    target = target[:, 1:]
+    target = target.contiguous()
+    return input, target
+
+
+def get_flatten_results(
+        input: Tensor, target: Tensor
+        ) -> Tuple[Tensor, Tensor]:
+    target = target.view(-1)
+    input = input.view(-1, input.shape[-1])
+    return input, target
