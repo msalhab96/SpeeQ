193ac2f08d03e181ba32494b34f29c1ed2a22176
msalhab96 <mahmoud@salhab.work>
2023-01-26 10:15:46 +0300

    ctc predictor added

--- predictors.py
+++ predictors.py
@@ -13,6 +13,7 @@ from utils.utils import load_state_dict
 class _ASRBasePredictor:
     """Implements the base ASR predictor
     """
+
     def __init__(
             self,
             speech_processor: IProcessor,
@@ -35,3 +36,46 @@ class _ASRBasePredictor:
         self.blank_id = self.tokenizer.special_tokens.blank_id
         self.sos = self.tokenizer.special_tokens.sos_id
         self.eos = self.tokenizer.special_tokens.eos_id
+
+
+class CTCPredictor(_ASRBasePredictor):
+    """Implements CTC based model predictor
+
+    Args:
+        speech_processor (IProcessor): The speech/file pre-processing
+            processor.
+        tokenizer_path (Union[str, Path]): The trained tokenizer path.
+        model_config (ModelConfig): The model configuration.
+        device (str): The device to map the operations to.
+    """
+
+    def __init__(
+            self,
+            speech_processor: IProcessor,
+            tokenizer_path: Union[str, Path],
+            model_config: ModelConfig, device: str,
+            *args, **kwargs
+    ) -> None:
+        super().__init__(
+            speech_processor, tokenizer_path, model_config, device
+        )
+
+    def predict(self, file_path: Union[Path, str]) -> str:
+        speech = self.speech_processor.execute(file_path)
+        speech = speech.to(self.device)
+        mask = torch.ones(1, speech.shape[1], dtype=torch.bool)
+        mask = mask.to(self.device)
+        preds, _ = self.model(speech, mask)  # M, 1, C
+        preds = torch.argmax(preds, dim=-1)
+        preds = preds.squeeze().tolist()
+        results = []
+        last_idx = -1
+        for item in preds:
+            if item not in [last_idx, self.blank_id]:
+                results.append(item)
+                last_idx = item
+        if results[0] == self.sos:
+            results = results[1:]
+        if results[-1] == self.eos:
+            results = results[:-1]
+        return ''.join(self.tokenizer.ids2tokens(results))
