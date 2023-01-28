bd8ec7cdcf84a0a3a3deaca2d440b9637da37ddc
msalhab96 <mahmoud@salhab.work>
2023-01-28 10:59:10 +0300

    TransducerPredictor added

--- predictors.py
+++ predictors.py
@@ -4,7 +4,9 @@ from typing import Union
 import torch
 
 from config import ModelConfig
-from constants import PREDS_KEY, TERMINATION_STATE_KEY
+from constants import (ENC_OUT_KEY, HIDDEN_STATE_KEY, PREDS_KEY,
+                       PREV_HIDDEN_STATE_KEY, SPEECH_IDX_KEY,
+                       TERMINATION_STATE_KEY)
 from data.registry import load_tokenizer
 from interfaces import IProcessor
 from models.registry import get_model
@@ -128,3 +130,53 @@ class Seq2SeqPredictor(_ASRBasePredictor):
             counter += 1
         results = state[PREDS_KEY][0, 1:-1].tolist()
         return ''.join(self.tokenizer.ids2tokens(results))
+
+
+class TransducerPredictor(_ASRBasePredictor):
+    """Implements transducer-Based models predictor
+
+    Args:
+        speech_processor (IProcessor): The speech/file pre-processing
+            processor.
+        tokenizer_path (Union[str, Path]): The trained tokenizer path.
+        model_config (ModelConfig): The model configuration.
+        device (str): The device to map the operations to.
+    """
+
+    def __init__(
+            self,
+            speech_processor: IProcessor,
+            tokenizer_path: Union[str, Path],
+            model_config: ModelConfig,
+            device: str
+    ) -> None:
+        super().__init__(
+            speech_processor,
+            tokenizer_path,
+            model_config,
+            device
+        )
+        # TODO: Add Beam search here
+
+    def predict(self, file_path: Union[Path, str]) -> str:
+        speech = self.speech_processor.execute(file_path)
+        speech = speech.to(self.device)
+        mask = torch.ones(1, speech.shape[1], dtype=torch.bool)
+        mask = mask.to(self.device)
+        state = {
+            PREDS_KEY: torch.LongTensor([[self.sos]]).to(self.device),
+            SPEECH_IDX_KEY: 0,
+            HIDDEN_STATE_KEY: None
+        }
+        length = 1
+        while state[SPEECH_IDX_KEY] < length:
+            state = self.model.predict(speech, mask, state)
+            length = state[ENC_OUT_KEY].shape[1]
+            last_pred = state[PREDS_KEY][0, -1].item()
+            if last_pred == self.blank_id:
+                state[SPEECH_IDX_KEY] += 1
+                state[HIDDEN_STATE_KEY] = state[PREV_HIDDEN_STATE_KEY]
+                # print(state[HIDDEN_STATE_KEY])
+                state[PREDS_KEY] = state[PREDS_KEY][:, :-1]
+        results = state[PREDS_KEY][0, :].tolist()
+        return ''.join(self.tokenizer.ids2tokens(results[1:]))
