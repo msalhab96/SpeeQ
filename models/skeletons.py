ed5209cafba929b8e307dc93666063ec7d174713
msalhab96 <mahmoud@salhab.work>
2023-01-30 20:00:16 +0300

    TransducerSkeleton added

--- models/skeletons.py
+++ models/skeletons.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from torch.nn import Module
 
+from models.transducers import BaseTransducer
+
 from .ctc import CTCModel
 
 
@@ -46,3 +48,52 @@ class CTCSkeleton(CTCModel):
         self.encoder = encoder
         if pred_net is not None:
             self.pred_net = pred_net
+
+
+class TransducerSkeleton(BaseTransducer):
+    """Builds the Transducer-based model skeleton
+
+    Args:
+        encoder (Module): The speech encoder (acoustic model), such that
+            the forward method of the encoder returns a tuple of the encoded
+            speech tensor and a length tensor for the encoded speech.
+        decoder (Module): The text decoder such that
+            the forward method of the decoder returns a tuple of the encoded
+            text tensor and a length tensor for the encoded text.
+        has_bnorm (bool): A flag indicates whether the encoder, the decoder, or
+            the join network has batch normalization.
+        join_net (Union[Module, None]): The join network. if provided
+            the forward of the join network expected to have no activation
+            function, and the results of shape [B, Ts, Tt, C], where B the
+            batch size, Ts is the speech sequence length, Tt is the text
+            sequence length, and C the number of classes. Default None.
+        feat_size (Union[Module, None]): Used if join_net parameter is not None
+            where it's the encoder and the decoder's output feature size.
+            Default None.
+        n_classes (Union[Module, None]): Used if join_net parameter is not None
+            where it's the number of the classes/characters to be predicted.
+    """
+
+    def __init__(
+            self,
+            encoder: Module,
+            decoder: Module,
+            has_bnorm: bool,
+            join_net: Union[Module, None] = None,
+            feat_size: Union[int, None] = None,
+            n_classes: Union[int, None] = None
+    ) -> None:
+        assert (feat_size is None) ^ (join_net is None)
+        if feat_size is not None:
+            assert n_classes is not None
+        args = [1, 1]
+        if feat_size is not None:
+            args[0] = feat_size
+        if n_classes is not None:
+            args[1] = n_classes
+        super().__init__(*args)
+        self.has_bnorm = has_bnorm
+        self.encoder = encoder
+        self.deocder = decoder
+        if join_net is not None:
+            self.join_net = join_net
