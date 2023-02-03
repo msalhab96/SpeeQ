01ce481550f435f9d5a5fcb5f2171eb0dee92c6b
Mahmoud Salhab <mahmoud@salhab.work>
2023-02-03 15:03:39 +0000

    get_mask added

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,3 +1,5 @@
+import torch
+
 IGNORE_USERWARNING = 'ignore::UserWarning'
 
 
@@ -8,3 +10,9 @@ def check_grad(result, model):
     loss.backward()
     for param in model.parameters():
         assert param.grad is not None
+
+
+def get_mask(seq_len: int, pad_lens: list):
+    mask = [[1] * (seq_len - item) + [0] * item for item in pad_lens]
+    mask = torch.BoolTensor(mask)
+    return mask
