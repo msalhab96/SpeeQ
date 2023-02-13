b55f31ed5f28d6a795f2cf2569f61dd52a27d061
msalhab96 <mahmoud@salhab.work>
2023-02-13 09:38:13 +0300

    SpeechProcessor added, closes #23

--- speeq/data/processors.py
+++ speeq/data/processors.py
@@ -1,5 +1,7 @@
 import random
 from abc import abstractmethod
+from pathlib import Path
+from typing import List, Optional, Union
 
 from speeq.interfaces import IProcess, IProcessor
 
@@ -59,4 +61,51 @@ class StochasticProcess(IProcess):
     def run(self, x):
         if self._shall_do:
             return self.func(x)
+        return x
+
+
+class SpeechProcessor(IProcessor):
+    """Speech processor the processing can be described as
+    spec_augmenter(spec_processor(audio_augmenter(audio_processor(file_path))))
+    if there's any feature extraction needed, it has to be part of the
+    spectrogram processor
+
+    Args:
+        audio_processor (OrderedProcessor): The audio processor, where
+            the input to it the audio file path.
+        audio_augmenter (Optional[Union[OrderedProcessor, StochasticProcessor]]):
+            The time domain augmentation processor. Default None.
+        spec_processor (Optional[OrderedProcessor]): The spectrogram processor,
+            where the input is the signal in the time domain, if any feature
+            extraction is needed it has to be part of it. Default None.
+        spec_augmenter (Optional[Union[OrderedProcessor, StochasticProcessor]]):
+            The frequency domain augmentation process. default None.
+    """
+
+    def __init__(
+        self,
+        audio_processor: OrderedProcessor,
+        audio_augmenter: Optional[Union[OrderedProcessor, StochasticProcessor]] = None,
+        spec_processor: Optional[OrderedProcessor] = None,
+        spec_augmenter: Optional[Union[OrderedProcessor, StochasticProcessor]] = None,
+    ) -> None:
+        super().__init__()
+        self.processors = []
+        self.__add(audio_processor)
+        self.__add(audio_augmenter)
+        self.__add(spec_processor)
+        self.__add(spec_augmenter)
+        if spec_augmenter is not None:
+            assert spec_processor is not None
+
+    def __add(
+        self, processor: Union[OrderedProcessor, StochasticProcessor, None]
+    ) -> None:
+        if processor is not None:
+            self.processors.append(processor)
+
+    def execute(self, file_path: Union[str, Path]):
+        x = file_path
+        for processor in self.processors:
+            x = processor.execute(x)
         return x
