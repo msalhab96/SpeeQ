ba7723f574b125b9523469d2a427f885dfe35038
msalhab96 <mahmoud@salhab.work>
2023-02-15 08:45:12 +0300

    csv file writer added

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,3 +1,6 @@
+import csv
+from typing import List
+
 import torch
 
 IGNORE_USERWARNING = "ignore::UserWarning"
@@ -15,3 +18,10 @@ def get_mask(seq_len: int, pad_lens: list):
     mask = [[1] * (seq_len - item) + [0] * item for item in pad_lens]
     mask = torch.BoolTensor(mask)
     return mask
+
+
+def create_csv_file(file_path, data: List[dict], encoding="utf-8", sep=","):
+    with open(file_path, "w", encoding=encoding) as f:
+        writer = csv.DictWriter(f, data[0].keys(), delimiter=sep)
+        writer.writeheader()
+        writer.writerows(data)
