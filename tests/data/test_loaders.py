6c2bfa120449255e3ca324d91614da13797f1989
msalhab96 <mahmoud@salhab.work>
2023-02-19 09:22:38 +0300

    TestSpeechTextDataset revamped

--- tests/data/test_loaders.py
+++ tests/data/test_loaders.py
@@ -1,14 +1,8 @@
-import csv
 import os
-from unittest import mock
 
 import pytest
 
-from speeq.constants import FileKeys
 from speeq.data import loaders
-from speeq.data.preprocessing import AudioLoader
-from speeq.data.processors import OrderedProcessor
-from speeq.data.tokenizers import CharTokenizer
 from tests.helpers import create_csv_file
 
 
@@ -24,28 +18,17 @@ class TestCSVDataset:
 
 
 class TestSpeechTextDataset:
-    def test(self, dict_csv_data, tmp_path):
-        encoding = "utf-8"
-        sep = ","
-        data = [item[FileKeys.text_key.value] for item in dict_csv_data]
-        file_path = os.path.join(tmp_path, "file.csv")
-        with open(file_path, "w", encoding=encoding) as f:
-            writer = csv.DictWriter(f, dict_csv_data[0].keys(), delimiter=sep)
-            writer.writeheader()
-            writer.writerows(dict_csv_data)
-        speech_processor = OrderedProcessor([AudioLoader(16000)])
-        text_processor = OrderedProcessor([])
-        char_tokenizer = CharTokenizer()
-        char_tokenizer.set_tokenizer(data)
-        dataset = loaders.SpeechTextDataset(
-            data_path=file_path,
-            tokenizer=char_tokenizer,
-            speech_processor=speech_processor,
-            text_processor=text_processor,
-            sep=sep,
-            encoding=encoding,
-        )
-        assert len(dataset) == len(dict_csv_data)
+    def test_wav(self, speech_text_dataset):
+        dataset = speech_text_dataset()
+        assert isinstance(dataset[0], tuple)
+        speech, speech_len, text, text_len = dataset[0]
+        assert speech.shape[1] == speech_len
+        assert text.shape[-1] == text_len
+        with pytest.raises(IndexError):
+            dataset[len(dataset)]
+
+    def test_spec(self, speech_text_dataset):
+        dataset = speech_text_dataset(use_mel_spec=True)
         assert isinstance(dataset[0], tuple)
         speech, speech_len, text, text_len = dataset[0]
         assert speech.shape[1] == speech_len
