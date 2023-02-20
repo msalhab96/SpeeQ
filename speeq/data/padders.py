7928153c901ff69fd0bb157f9ab35996acf70721
msalhab96 <mahmoud@salhab.work>
2023-02-20 21:29:54 +0300

    module level docstring added, docstrings revamped

--- speeq/data/padders.py
+++ speeq/data/padders.py
@@ -1,4 +1,36 @@
-from typing import Union
+"""The `padders` module provides two classes for padding input sequences:
+`DynamicPadder` and `StaticPadder`.
+
+`DynamicPadder` pads an input sequence along a specified dimension to match
+the maximum sequence length, while `StaticPadder` is a subclass of
+`DynamicPadder` that also allows the user to specify the maximum length
+of the sequence to pad to.
+
+Both classes have a `pad` method that accepts an input tensor and the maximum
+length to pad to, and returns the padded tensor and the length of the padding added.
+
+
+Usage:
+
+
+.. code-block:: python
+
+    import torch
+    from speeq.data.padders import DynamicPadder, StaticPadder
+
+    # create a dummy input
+    input_tensor = torch.randn(1, 3, 7)
+
+    # Example usage of DynamicPadder
+    dynamic_padder = DynamicPadder(dim=1, pad_val=0)
+    padded_tensor, padding_length = dynamic_padder.pad(input_tensor, max_len=10)
+
+    # Example usage of StaticPadder
+    static_padder = StaticPadder(dim=1, pad_val=0, max_len=10)
+    padded_tensor, padding_length = static_padder.pad(input_tensor)
+
+"""
+from typing import Tuple, Union
 
 import torch
 from torch import Tensor
@@ -11,8 +43,10 @@ class DynamicPadder(IPadder):
 
     Args:
         dim (int): The dimension to do the padding across.
+
         pad_val (Union[int, Tensor, float]): The padding value that
         will be used to fill the padding sequence.
+
         left_pad (int): The side to pad the padding sequence to.
     """
 
@@ -29,7 +63,19 @@ class DynamicPadder(IPadder):
         self.left_pad = left_pad
         self.pad_val = pad_val
 
-    def pad(self, x: Tensor, max_len: int):
+    def pad(self, x: Tensor, max_len: int) -> Tuple[Tensor, int]:
+        """Pads the input tensor to match the specified maximum length along the
+        pre-defined dimension.
+
+        Args:
+            x (Tensor): The input tensor to be padded.
+
+            max_len (int): The maximum length to pad the input tensor to.
+
+        Returns:
+            Tuple[Tensor, int]: A tuple containing the padded tensor and the
+            length of the padding added.
+        """
         seq_len = x.shape[self.dim]
         pad_len = max_len - seq_len
         assert pad_len >= 0
@@ -47,14 +93,19 @@ class DynamicPadder(IPadder):
 
 
 class StaticPadder(DynamicPadder):
-    """Pads the input sequence across a dim for the maximum length
+    """A subclass of `DynamicPadder` that pads an input sequence to match
+    a pre-defined maximum length along a specified dimension.
+
 
     Args:
-        dim (int): The dimension to do the padding across.
-        pad_val (Union[int, Tensor, float]): The padding value that
-        will be used to fill the padding sequence.
-        max_len (int): The maximum sequence length.
-        left_pad (int): The side to pad the padding sequence to.
+        dim (int): The dimension to pad across.
+
+        pad_val (Union[int, Tensor, float]): The value used to fill the padded sequence.
+
+        max_len (int): The maximum length of the sequence to pad to.
+
+        left_pad (int): The side to which the sequence will be padded.
+
     """
 
     def __init__(
@@ -70,4 +121,14 @@ class StaticPadder(DynamicPadder):
         self.max_len = max_len
 
     def pad(self, x: Tensor, *args, **kwargs):
+        """Pads the input tensor to match the specified maximum length along the
+        pre-defined dimension.
+
+        Args:
+            x (Tensor): The input tensor to be padded.
+
+        Returns:
+            Tuple[Tensor, int]: A tuple containing the padded tensor and the
+            length of the padding added.
+        """
         return super().pad(x, self.max_len)
