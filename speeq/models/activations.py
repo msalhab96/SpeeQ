24ca81554309a023e4748962bfc4681ab3334a39
msalhab96 <mahmoud@salhab.work>
2023-02-22 15:24:32 +0300

    docstrings updated, module level docstring added

--- speeq/models/activations.py
+++ speeq/models/activations.py
@@ -1,3 +1,11 @@
+"""
+This module contains custom activation functions that can be used in PyTorch models.
+
+Available functions:
+
+- Sigmax: Implements the custom activation function for attention.
+- CReLu: Implements the Clipped Rectified Linear Unit (CReLu) activation function.
+"""
 import torch
 from torch import Tensor, nn
 
@@ -5,6 +13,11 @@ from torch import Tensor, nn
 class Sigmax(nn.Module):
     """Implements the custom activation function for attention
     proposed in https://arxiv.org/abs/1506.07503
+
+    Args:
+
+        dim (int): The dimension to apply the activation function on.
+
     """
 
     def __init__(self, dim: int = -1) -> None:
@@ -12,19 +25,27 @@ class Sigmax(nn.Module):
         self.dim = dim
 
     def forward(self, x: Tensor) -> Tensor:
+        """Passes the input tensor `x` through the Sigmax activation function.
+
+        Args:
+            x (Tensor): The input tensor.
+
+        Returns:
+            Tensor: The result tensor after applying the activation function.
+        """
         sigma = torch.sigmoid(x)
         sum = sigma.sum(dim=self.dim, keepdim=True)
         return sigma / sum
 
 
 class CReLu(nn.Module):
-    """clipped rectified-linear unit, can be described as
-    min{max{0, x}, max_value}
-
+    """implements the Clipped Rectified Linear Unit (CReLu) activation function
     as described in: https://arxiv.org/abs/1412.5567
 
     Args:
-        max_val (int): the maximum clipping value.
+
+        max_val (int): The maximum value for clipping
+
     """
 
     def __init__(self, max_val: int) -> None:
@@ -32,4 +53,13 @@ class CReLu(nn.Module):
         self.max_val = max_val
 
     def forward(self, x: Tensor) -> Tensor:
+        """Passes the input tensor `x` through the Clipped Rectified Linear Unit
+        (CReLu) activation function.
+
+        Args:
+            x (Tensor): The input tensor.
+
+        Returns:
+            Tensor: The result tensor after applying the activation function.
+        """
         return torch.clamp(x, min=0, max=self.max_val)
