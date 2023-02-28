1661f66874ca2b132699c5a9b9876e6d314d9638
msalhab96 <mahmoud@salhab.work>
2023-02-28 21:12:35 +0300

    VGGTransformerTransducer model added

--- speeq/models/transducers.py
+++ speeq/models/transducers.py
@@ -23,7 +23,12 @@ from speeq.constants import (
 )
 
 from .decoders import TransducerRNNDecoder
-from .encoders import ConformerEncoder, ContextNetEncoder, RNNEncoder
+from .encoders import (
+    ConformerEncoder,
+    ContextNetEncoder,
+    RNNEncoder,
+    VGGTransformerEncoder,
+)
 
 
 class _BaseTransducer(nn.Module):
@@ -276,4 +281,105 @@ class ContextNet(_BaseTransducer):
             if isinstance(out_channels, list)
             else out_channels,
             rnn_type=rnn_type,
+        )
+
+
+class VGGTransformerTransducer(RNNTransducer):
+    """Implements the Transformer-Transducer model as described in
+    https://arxiv.org/abs/1910.12977
+
+    Args:
+
+        in_features (int): The input feature size.
+
+        n_classes (int): The number of classes/vocabulary.
+
+        emb_dim (int): The embedding layer's size.
+
+        n_layers (int): The number of transformer encoder layers with truncated
+        self attention.
+
+        rnn_type (str): The RNN type.
+
+        n_vgg_blocks (int): The number of VGG blocks to use.
+
+        n_conv_layers_per_vgg_block (List[int]): A list of integers that specifies the number
+        of convolution layers in each block.
+
+        kernel_sizes_per_vgg_block (List[List[int]]): A list of lists that contains the
+        kernel size for each layer in each block. The length of the outer list
+        should match `n_vgg_blocks`, and each inner list should be the same length
+        as the corresponding block's number of layers.
+
+        n_channels_per_vgg_block (List[List[int]]): A list of lists that contains the
+        number of channels for each convolution layer in each block. This argument
+        should also have length equal to `n_vgg_blocks`, and each sublist should
+        have length equal to the number of layers in the corresponding block.
+
+        vgg_pooling_kernel_size (List[int]): A list of integers that specifies the size
+        of the max pooling layer in each block. The length of this list should be
+        equal to `n_vgg_blocks`.
+
+        d_model (int): The model dimensionality.
+
+        ff_size (int): The feed forward inner layer dimensionality.
+
+        h (int): The number of heads in the attention mechanism.
+
+        left_size (int): The size of the left window that each time step is
+        allowed to look at.
+
+        right_size (int): The size of the right window that each time step is
+        allowed to look at.
+
+        p_dropout (float): The dropout rate.
+
+        masking_value (float, optional): The value to use for masking padded
+        elements. Defaults to -1e15.
+    """
+
+    def __init__(
+        self,
+        in_features: int,
+        n_classes: int,
+        emb_dim: int,
+        n_layers: int,
+        rnn_type: str,
+        n_vgg_blocks: int,
+        n_conv_layers_per_vgg_block: List[int],
+        kernel_sizes_per_vgg_block: List[List[int]],
+        n_channels_per_vgg_block: List[List[int]],
+        vgg_pooling_kernel_size: List[int],
+        d_model: int,
+        ff_size: int,
+        h: int,
+        left_size: int,
+        right_size: int,
+        p_dropout: float,
+        masking_value: int = -1e15,
+    ) -> None:
+        super().__init__(
+            in_features=in_features,
+            n_classes=n_classes,
+            emb_dim=emb_dim,
+            n_layers=1,
+            hidden_size=d_model,
+            bidirectional=False,
+            rnn_type=rnn_type,
+            p_dropout=p_dropout,
+        )
+        self.encoder = VGGTransformerEncoder(
+            in_features=in_features,
+            n_layers=n_layers,
+            n_vgg_blocks=n_vgg_blocks,
+            n_conv_layers_per_vgg_block=n_conv_layers_per_vgg_block,
+            kernel_sizes_per_vgg_block=kernel_sizes_per_vgg_block,
+            n_channels_per_vgg_block=n_channels_per_vgg_block,
+            vgg_pooling_kernel_size=vgg_pooling_kernel_size,
+            d_model=d_model,
+            ff_size=ff_size,
+            h=h,
+            left_size=left_size,
+            right_size=right_size,
+            masking_value=masking_value,
         )
