5a872aae8e396b532e57ce8354ad06331fb9ba69
msalhab96 <mahmoud@salhab.work>
2023-02-28 20:26:31 +0300

    VGGTransformerEncoder added

--- speeq/models/encoders.py
+++ speeq/models/encoders.py
@@ -37,6 +37,9 @@ from .layers import (
     RowConv1D,
     SpeechTransformerEncLayer,
     SqueezeformerBlock,
+    TransformerEncLayer,
+    TransformerEncLayerWithAttTruncation,
+    VGGTransformerPreNet,
 )
 
 
@@ -1265,4 +1268,113 @@ class ContextNetEncoder(nn.Module):
         for layer in self.layers:
             out, lengths = layer(out, lengths)
         out = out.transpose(-1, -2)  # [B, M, d']
+        return out, lengths
+
+
+class VGGTransformerEncoder(nn.Module):
+    """Implements the VGGTransformer encoder as described in
+    https://arxiv.org/abs/1910.12977
+
+    Args:
+
+        in_features (int): The input feature size.
+
+        n_layers (int): The number of transformer encoder layers with truncated
+        self attention.
+
+        n_vgg_blocks (int): The number of VGG blocks to use.
+
+        n_conv_layers_per_vgg_block (List[int]): A list of integers that specifies the number
+        of convolution layers in each block.
+
+        kernel_sizes_per_vgg_block (List[List[int]]): A list of lists that contains the
+        kernel size for each layer in each block. The length of the outer list
+        should match `n_vgg_blocks`, and each inner list should be the same length
+        as the corresponding block's number of layers.
+
+        n_channels_per_vgg_block (List[List[int]]): A list of lists that contains the
+        number of channels for each convolution layer in each block. This argument
+        should also have length equal to `n_vgg_blocks`, and each sublist should
+        have length equal to the number of layers in the corresponding block.
+
+        vgg_pooling_kernel_size (List[int]): A list of integers that specifies the size
+        of the max pooling layer in each block. The length of this list should be
+        equal to `n_vgg_blocks`.
+
+        d_model (int): The model dimensionality.
+
+        ff_size (int): The feed forward inner layer dimensionality.
+
+        h (int): The number of heads in the attention mechanism.
+
+        left_size (int): The size of the left window that each time step is
+        allowed to look at.
+
+        right_size (int): The size of the right window that each time step is
+        allowed to look at.
+
+        masking_value (float, optional): The value to use for masking padded
+        elements. Defaults to -1e15.
+    """
+
+    def __init__(
+        self,
+        in_features: int,
+        n_layers: int,
+        n_vgg_blocks: int,
+        n_conv_layers_per_vgg_block: List[int],
+        kernel_sizes_per_vgg_block: List[List[int]],
+        n_channels_per_vgg_block: List[List[int]],
+        vgg_pooling_kernel_size: List[int],
+        d_model: int,
+        ff_size: int,
+        h: int,
+        left_size: int,
+        right_size: int,
+        masking_value: int = -1e15,
+    ) -> None:
+        super().__init__()
+        self.pre_net = VGGTransformerPreNet(
+            in_features=in_features,
+            n_vgg_blocks=n_vgg_blocks,
+            n_layers_per_block=n_conv_layers_per_vgg_block,
+            kernel_sizes_per_block=kernel_sizes_per_vgg_block,
+            n_channels_per_block=n_channels_per_vgg_block,
+            pooling_kernel_size=vgg_pooling_kernel_size,
+            d_model=d_model,
+        )
+        self.enc_layers = nn.ModuleList(
+            [
+                TransformerEncLayerWithAttTruncation(
+                    d_model=d_model,
+                    ff_size=ff_size,
+                    h=h,
+                    left_size=left_size,
+                    right_size=right_size,
+                    masking_value=masking_value,
+                )
+                for _ in range(n_layers)
+            ]
+        )
+
+    def forward(self, x: Tensor, mask: Tensor) -> Tuple[Tensor, Tensor]:
+        """Passes the input `x` through the encoder layers.
+
+        Args:
+
+            x (Tensor): The input speech tensor of shape [B, M, d]
+
+            mask (Tensor): The input boolean mask of shape [B, M], where it's True
+            if there is no padding.
+
+        Returns:
+
+            Tuple[Tensor, Tensor]: A tuple where the first element is the encoded speech of shape
+            [B, M, F] and the second element is the lengths of shape [B].
+        """
+        lengths = mask.sum(dim=-1)
+        out, lengths = self.pre_net(x, lengths)
+        mask = get_mask_from_lens(lengths=lengths, max_len=out.shape[1])
+        for layer in self.enc_layers:
+            out = layer(out, mask)
         return out, lengths
