bbefb0fa1d5fa55fab9d5ae0b9b67638b8b3f3a6
Mahmoud Salhab <mahmoud@salhab.work>
2023-03-03 21:36:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from speeq._version import __version__
 with open("requirements.txt", "r", encoding="utf-8") as f:
     requirements = f.readlines()
 
+with open("README.md", "r", encoding="utf-8") as f:
+    description = f.read()
+    
 setup(
     name="speeq",
     version=__version__,
@@ -12,8 +15,12 @@ setup(
     author_email="mahmoud@salhab.work",
     url="https://github.com/msalhab96/SpeeQ",
     install_requires=requirements,
+    long_description = description,
+    long_description_content_type='text/markdown',
     license="LICENSE",
     packages=["speeq"],
     setup_requires=["wheel"],
     python_requires=">=3.7",
+    keywords=["speeq", "asr", "acoustic_modeling", "speech_recognition", "pytorch"],
+    
 )
